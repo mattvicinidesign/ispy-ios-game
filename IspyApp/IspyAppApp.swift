0f29f862264eaaefb71f3e83a9683f4d6f470c21
Matt Vicini <vicinima@gmail.com>
2026-01-15 10:05:45 -0600

    Initial Commit

---
+++ IspyApp/IspyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IspyAppApp.swift
+//  IspyApp
+//
+//  Created by Matt on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct IspyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
